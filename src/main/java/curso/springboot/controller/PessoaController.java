66f387915a58df49f1fb82c6e4606b688cb8597e
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-08-18 21:42:59 -0300

    SpringBoot-MVC

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -4,9 +4,11 @@ import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PessoaController {
@@ -20,10 +22,24 @@ public class PessoaController {
     }
 
     @PostMapping(value="/salvarpessoa")
-    public String salvar(Pessoa pessoa){
+    public ModelAndView salvar(Pessoa pessoa){
         pessoaRepository.save(pessoa);
-        return "cadastro/cadastropessoa";
+
+        //Após o cadastro irá listas as pessoas já cadastradas
+        ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+        Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+        andView.addObject("pessoas", pessoasIt);
+
+        return andView;
     }
 
+    @GetMapping(value="/listapessoas")
+    public ModelAndView pessoas(){
+            ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+            Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+            andView.addObject("pessoas", pessoasIt);
+
+            return andView;
+    }
 
 }
